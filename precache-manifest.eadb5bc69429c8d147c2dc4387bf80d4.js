19a2477d6b3bdef8badcb24cac5a058d98d3018b
Trevor Wolfe <trevo1992@gmail.com>
2020-09-17 12:44:37 -0700

    Updates

---
+++ precache-manifest.eadb5bc69429c8d147c2dc4387bf80d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f511dbfa8f7de61ea4bc694e1a17a42",
+    "url": "/ReactTesting/index.html"
+  },
+  {
+    "revision": "e3999dcc23a8956f342f",
+    "url": "/ReactTesting/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f500de02339613c7accf",
+    "url": "/ReactTesting/static/js/2.f7e164c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactTesting/static/js/2.f7e164c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3999dcc23a8956f342f",
+    "url": "/ReactTesting/static/js/main.33ed78ed.chunk.js"
+  },
+  {
+    "revision": "3cafc395b7445216418c",
+    "url": "/ReactTesting/static/js/runtime-main.05782e29.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactTesting/static/media/logo.5d5d9eef.svg"
+  }
+]);
